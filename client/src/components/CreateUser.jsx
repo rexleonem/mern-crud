afdfc96ed606bd717b17e4815ce1efd63cdfccfb
Rex Leonem <rexleonem1@gmail.com>
2024-05-16 00:51:51 +0100

    Create Pages

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function CreateUser() {
   return (
-    <div>CreateUser</div>
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+       <div className="w500 bg-white rounded p-3">
+        <h1>Create User</h1>
+        <form>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">Name</label>
+                <input type="text" className="form-control" id="name" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="email" className="form-label">Email</label>
+                <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="age" className="form-label">Age</label>
+                <input type="number" className="form-control" id="age" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+        </div>
+    </div>
   )
 }
--- client/src/components/Users.jsx
+++ client/src/components/Users.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Users() {
+    const [users, setUsers] = useState([{
+        name: 'John',
+        email: 'john',
+        age: 20,
+        id: 1
+    }])
   return (
-    <div>Users</div>
+    <div className='d-flex vh-100 bg-primary justify-content-center align-items-center'>
+        <div className="w500 bg-white rounded p-3">
+            <Link to='/create' className='btn btn-primary'>Create User</Link>
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Age</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        users.map(user => {
+                            return (
+                                <tr key={user.id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
+                                    <td>
+                                    {/* <Link to={`/update/${user.id}`} className='btn btn-primary'>Update</Link> */}
+                                        <Link to='/update' className='btn btn-primary'>Update</Link>
+                                        <button className='btn btn-danger'>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    </div>
   )
 }
